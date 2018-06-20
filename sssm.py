1321a864f97ba2ea58f53065c97c60f11cf15cfc
Zibi <zibi667@gmail.com>
2018-06-21 00:02:54 +0100

    Stock and Market calculations

--- sssm.py
+++ sssm.py
@@ -1,15 +1,17 @@
 import pandas as pd
-
+import time
+import numpy as np
 
 class StockMarket:
     def __init__(self):
         self.stocks = pd.DataFrame(columns=["Symbol", "IsPreferred", "LastDividend", "FixedDividend", "ParValue"])
-        self.trades = pd.DataFrame(columns=["Symbol", "Timestamp", "Quantity", "Price", "IsBuy"])
+        self.trades = pd.DataFrame(columns=["Symbol", "UnixTimestamp", "Quantity", "Price", "IsBuy"])
 
     def get_stock(self, stock_symbol):
         for _, stock in self.stocks.iterrows():
             if stock["Symbol"] == stock_symbol:
                 return stock
+        raise KeyError("Stock symbol not found in data")
 
     def add_stock(self, symbol, is_preferred, last_dividend, fixed_dividend, par_value):
         for s in self.stocks.Symbol:
@@ -25,16 +27,71 @@ class StockMarket:
 
 
 class StockCalculator:
-    def __init__(self, stock_symbol, stock_market):
+
+    class ArgumentError(Exception):
+        pass
+
+    def __init__(self, stock_symbol=None, stock_market=None):
         self.stockSymbol = stock_symbol
         self.stockMarket = stock_market
 
     def get_dividend_yield(self, price):
-        pass
+        if price <= 0:
+            raise StockCalculator.ArgumentError("Price has to be greater than 0")
 
+        try:
+            stock = self.stockMarket.get_stock(self.stockSymbol)
+        except KeyError:
+            return None
 
-    def get_pe_ratio(self, price):
-        pass
+        if stock["IsPreferred"]:
+            return self._calculate_common_dividend_yield(stock["LastDividend"], price)
+        return
+
+    def _calculate_common_dividend_yield(self, last_dividend, price):
+        return float(last_dividend)/price
+
+    def _calculate_preferred_dividend_yield(self, fixed_dividend, par_value, price):
+        return (fixed_dividend * par_value)/100 * par_value/price
+
+    def get_pe_ratio(self, price, dividend=None):
+        if not dividend:
+            dividend = self.get_dividend_yield(price)
+        return price/dividend
 
     def get_volume_weighted_stock_price(self, time_frame_minutes=5):
-        pass
+        trades = self.stockMarket.get_trades(self.stockSymbol)
+        time_start = int(time.time()) - time_frame_minutes * 60
+        last_trades = trades[trades.UnixTimestamp > time_start]
+
+        total_volume = 0
+        weight = 0
+
+        for _, trade in last_trades.iterrows():
+            total_volume += trade["Quantity"] * trade["Price"]
+            weight += trade["Quantity"]
+
+        if weight == 0:
+            return 0
+
+        return total_volume/weight
+
+
+class MarketCalculator:
+    def __init__(self, market):
+        self.market = market
+
+    def get_all_share_index(self, time_frame_minutes=5):
+        volume_weighted_stock_prices = []
+        stock_calculator = StockCalculator(stock_market=self.market, stock_symbol="")
+        for _, stock in self.market.stocks.iterrows():
+            stock_calculator.stockSymbol = stock["Symbol"]
+            vwsp = stock_calculator.get_volume_weighted_stock_price(time_frame_minutes)
+            volume_weighted_stock_prices.append(vwsp)
+
+        return self._calculate_geometric_mean(volume_weighted_stock_prices)
+
+    def _calculate_geometric_mean(self, array):
+        np_array = np.array(array)
+        return np_array.prod() ** (1.0/len(np_array))
+
--- tests.py
+++ tests.py
@@ -1,6 +1,44 @@
 import unittest
+from unittest.mock import MagicMock
+
 import sssm
 import pandas
+import time
+
+from sssm import StockCalculator
+
+
+class TestsDataSource:
+
+    @staticmethod
+    def create_dummy_market_with_trades_from_last_10_minutes():
+        market = sssm.StockMarket()
+
+        stock = ["AAA", False, 10, 0, 100]
+        market.add_stock(*stock)
+        stock[0] = "BBB"
+        market.add_stock(*stock)
+
+        timestamp_now = int(time.time())
+
+        trade = ["AAA", timestamp_now, 1, 100, 1]
+        market.register_trade(*trade)
+
+        trade[1] = timestamp_now - 60
+        trade[3] = 200
+        market.register_trade(*trade)
+
+        trade[1] = timestamp_now - 600
+        trade[3] = 1000
+        market.register_trade(*trade)
+
+        trade[0] = "BBB"
+        trade[1] = timestamp_now
+        market.register_trade(*trade)
+
+        return market
+
+
 
 class StockMarketTests(unittest.TestCase):
     example_trade_entry_1 = ("AAA", 11, 1, 1.5, False)
@@ -14,7 +52,7 @@ class StockMarketTests(unittest.TestCase):
                          "There should be only 1 trade entry, found: {0}".format(len(market.trades)))
         trade_entry = market.trades.iloc[0]
         self.assertEqual(trade_entry["Symbol"], StockMarketTests.example_trade_entry_1[0])
-        self.assertEqual(trade_entry["Timestamp"], StockMarketTests.example_trade_entry_1[1])
+        self.assertEqual(trade_entry["UnixTimestamp"], StockMarketTests.example_trade_entry_1[1])
         self.assertEqual(trade_entry["Quantity"], StockMarketTests.example_trade_entry_1[2])
         self.assertEqual(trade_entry["Price"], StockMarketTests.example_trade_entry_1[3])
         self.assertEqual(trade_entry["IsBuy"], StockMarketTests.example_trade_entry_1[4])
@@ -62,10 +100,67 @@ class StockMarketTests(unittest.TestCase):
         self.assertIsInstance(actual_result, pandas.Series)
         self.assertListEqual(expected_result, list(actual_result))
 
+    def test_getting_nonexistent_stock_returns_none(self):
+        market = sssm.StockMarket()
+        with self.assertRaises(KeyError):
+            market.get_stock("AAA")
 
 
+class StockCalculatorTests(unittest.TestCase):
+    def test_common_dividend_yield_is_calculated_correctly(self):
+        calc = sssm.StockCalculator()
+        self.assertEqual(calc._calculate_common_dividend_yield(100, 20), 5)
+        self.assertEqual(calc._calculate_common_dividend_yield(5, 2), 2.5)
+
+    def test_preferred_dividend_yield_is_calculated_correctly(self):
+        calc = sssm.StockCalculator()
+        self.assertEqual(calc._calculate_preferred_dividend_yield(10, 100, 200), 5)
+        self.assertEqual(calc._calculate_preferred_dividend_yield(10, 20, 200), 0.2)
+
+    def test_dividend_returns_none_if_stock_symbol_is_incorrect(self):
+        market = sssm.StockMarket()
+        calc = sssm.StockCalculator(stock_symbol="NOPE", stock_market=market)
+        self.assertIsNone(calc.get_dividend_yield(1))
+
+    def test_dividend_raises_error_if_price_is_not_greater_than_zero(self):
+        calc = sssm.StockCalculator(stock_symbol="")
+        with self.assertRaises(sssm.StockCalculator.ArgumentError):
+            calc.get_dividend_yield(0)
+
+        with self.assertRaises(sssm.StockCalculator.ArgumentError):
+            calc.get_dividend_yield(-1)
+
+    def test_pe_ratio_is_calculated_correctly(self):
+        calc = sssm.StockCalculator()
+        dividend_mock = MagicMock(return_value=2)
+        calc.get_dividend_yield = dividend_mock
+
+        self.assertEqual(calc.get_pe_ratio(100), 50)
+        calc.get_dividend_yield.assert_called_once()
+
+        self.assertEqual(calc.get_pe_ratio(100, 20), 5)
+        calc.get_dividend_yield.assert_called_once()
+
+    def test_volume_weighted_stock_price_is_calculated_correctly(self):
+        market = TestsDataSource.create_dummy_market_with_trades_from_last_10_minutes()
+
+        calc = sssm.StockCalculator("AAA", market)
+        result = calc.get_volume_weighted_stock_price(time_frame_minutes=5)
+        expected = 150
+        self.assertEqual(result, expected, "Volume weighted stock price is incorrect")
+
+    def test_volume_weighted_stock_price_returns_zero_if_no_trades_were_made(self):
+        market = sssm.StockMarket()
+        calc = sssm.StockCalculator("AAA", market)
+        self.assertEqual(calc.get_volume_weighted_stock_price(), 0)
 
 
+class MarketCalculatorTests(unittest.TestCase):
+    def test_all_share_index_is_calculated_correctly(self):
+        market = TestsDataSource.create_dummy_market_with_trades_from_last_10_minutes()
+        market_calc = sssm.MarketCalculator(market)
+        expected = 387.2983346207417
+        self.assertEqual(market_calc.get_all_share_index(), expected)
 
 
 if __name__ == '__main__':
